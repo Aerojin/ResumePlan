5f0cf2de74081cf48517c10f31b9f2a3135d17a5
aerojin <aerojin@126.com>
2015-02-11 23:52:55 +0800

    增加分享组件

---
+++ js/kit/we.share.js
@@ -0,0 +1,83 @@
+﻿; (function (WE, jQuery, Backbone) {
+
+    var superClass = WE.View.ViewBase;
+    var _class = "WE.Share";
+
+    WE.namespace(_class, superClass.extend({
+
+        name: _class,
+
+        template: [
+            '<div class="windowBox" id="<%-cid %>-share" style="width:480px;">',
+                '<a href="javascript:void(0);" id="<%-cid %>-close" class="i_icoCloseW i_icoCloseWBtn"></a>',
+                '<h2>分享</h2>',
+                '<ul class="shareList clearfix">',
+                    '<li><a href="javascript:void(0);" shareid="qzone" class="i_icoSone"></a></li>',
+                    '<li><a href="javascript:void(0);" shareid="tsina" class="i_icoStwo"></a></li>',
+                    '<li><a href="javascript:void(0);" shareid="tqq" class="i_icoSthrere"></a></li>',
+                    '<li><a href="javascript:void(0);" shareid="linkedin" class="i_icoSfour"></a></li>',
+                    '<li><a href="javascript:void(0);" shareid="weixin" class="i_icoSfive"></a></li>',
+                '</ul>',
+            '</div>'
+        
+        ].join("\n"),
+
+        jiaThisAPI: "http://www.jiathis.com/send/?webid=<%-shareid%>&url=<%-url%>&title=<%-title%>&summary=<%-summary%>&imageUrl=<%-imageUrl%>",
+
+        initialize: function (options) {
+            this.container = options.container || $('body');
+            this.onClose = options.onClose || this.onClose;
+
+            this.render();
+            this.initEvents();
+        },
+
+        initEvents: function () {
+            var _this = this;
+
+            this.ui.button.click(function() {
+                var shareID = $(this).attr("shareid");
+
+                _this.share(shareID);
+            });
+
+            this.ui.btnClose.click(function () {
+                _this.ui.wrap.remove();
+                _this.onClose();
+            });
+        },
+
+        render: function () {
+            
+            var tamplate = _.template(this.template);
+                tamplate = tamplate({cid: this.cid});
+
+            this.ui = {};
+            this.ui.wrap = $(tamplate);
+            this.ui.button = this.ui.wrap.find("ul a");
+            this.ui.btnClose = this.getCidEl("close", this.ui.wrap);
+
+            this.container.append(this.ui.wrap);
+        },
+
+        share: function (shareID) {            
+            var url = _.template(this.jiaThisAPI);
+                url = url({
+                    shareid: shareID,
+                    url: "http://www.baidu.com",
+                    title: "简历计划",
+                    summary: "简历计划",
+                    imageUrl: ""
+                });
+
+            window.open(url);
+        },
+
+        onClose: function () {
+        
+        } 
+
+    }));
+
+
+})(WE, jQuery, Backbone);
--- js/src/we.user.center.view.js
+++ js/src/we.user.center.view.js
@@ -10,7 +10,7 @@
         defaults: function () {
             return {
                 date: new Date(),
-                percent: 0,
+                percent: 0
             };
         },      
 
@@ -54,17 +54,24 @@
                 var percent = this.get("percent");
 
                 _this.ui.lblPercent.text(percent + "%");
-                _this.ui.spanPercent.css({width: percent});
+                _this.ui.spanPercent.css({width: percent + "%"});
             });
 
             this.model.on("change:date", function () {
-                var date = this.get("date");
-
-
+                var date = this.get("date") || new Date();
+                    date = WE.Date.format("yyyy-MM-dd", date);
+                
+                _this.ui.spanTime.text(date);
             });
         },
 
         initPageEvents: function () {
+            var _this = this;
+
+            this.ui.btnShare.click(function () {
+                _this.openShare();
+            });
+            
 
         },
 
@@ -72,19 +79,31 @@
             
             this.ui = {};
             this.ui.main = $("#main");
+            this.ui.btnShare = this.ui.main.find("#btn-share");
             this.ui.spanTime = this.ui.main.find("#span-time");
             this.ui.spanPercent = this.ui.main.find("#span-percent");
             this.ui.lblPercent = this.ui.main.find("#lbl-percent");
             this.ui.spanPercent = this.ui.main.find("#span-percent");
             
+            this.model.set({
+                percent: 33,
+                date: new Date()
+            });
+        },
+        openShare: function () {
             
+            this.share = new WE.Share({
+                onClose: function () {
+                
+                }
+            });        
         }
 
     }));
 
     $(function () {
-        var model = WE.User.Center.Model();
-        var view = WE.User.Center.View({model: model});
+        var model = new WE.User.Center.Model();
+        var view = new WE.User.Center.View({model: model});
     });
 
 
